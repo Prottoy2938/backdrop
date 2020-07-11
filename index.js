0755ba2e4ff846f845197b136a4d0491cea39cd4
Prottay Rudra <prottoyrudra7777@gmail.com>
2020-07-12 02:05:41 +0600

    Updated dom element handle structure - moved all the event element to a new container and added that after the video element - cleaned up some code

--- index.js
+++ index.js
@@ -1,8 +1,6 @@
-const imageUpload = document.createElement("input"); //initializing the element, not showing it yet [look at `gotResult` function]
-const captureFrameBtn = document.createElement("button"); //initializing the element, not showing it yet [look at `gotResult` function]
-imageUpload.type = "file";
-imageUpload.accept = "image/*";
-captureFrameBtn.innerText = "Capture Images";
+const eventContainer = document.querySelector("#event-container"); //initializing the element, not showing it yet [look at `gotResult` function]
+const imageUpload = document.querySelector("#cs1-image-upload");
+const captureFrame = document.querySelector("#capture-frame");
 
 let video;
 let uNet;
@@ -35,14 +33,13 @@ function gotResult(error, result) {
     //doing stuff after the initial uNet model has loaded and working
     uNetActive = true;
     bg = loadImage("./assets/initial-background.jpg");
-    const canvas = document.querySelector("video"); //getting the canvas after its created by p5js
-
-    //adding those element on the dom now
-    canvas.parentNode.insertBefore(imageUpload, canvas.nextSibling);
-    canvas.parentNode.insertBefore(captureFrameBtn, canvas.nextSibling);
+    const video = document.querySelector("video"); //getting the video after its created by p5js
+    video.parentNode.insertBefore(eventContainer, video.nextSibling); //inserting the eventContainer after the video element [https://stackoverflow.com/questions/4793604/how-to-insert-an-element-after-another-element-in-javascript-without-using-a-lib]
+    eventContainer.style.display = "block";
   }
-  segmentationImage = result.backgroundMask; // set the result to the global segmentation variable
-  uNet.segment(video, gotResult); // Continue asking for a segmentation image
+
+  segmentationImage = result.backgroundMask;
+  uNet.segment(video, gotResult);
 }
 
 //adding the dom elements, from p5js
@@ -51,14 +48,14 @@ function draw() {
   image(segmentationImage, 0, 0, width, height);
 }
 
-//Image Upload handler
+//Image upload handler
 imageUpload.addEventListener("change", (e) => {
   const imgSrc = window.URL.createObjectURL(e.target.files[0]);
   bg = loadImage(imgSrc); //changing background to imported image
   background(bg);
 });
 
-//Image Download
+//Image download handler
 captureFrameBtn.addEventListener("click", () => {
   saveFrames("out", "png", 1, 25, (data) => {
     print(data);
